c2a0e863edcff1bb3cc8e1ca4cd6cdab8d64e6ed
Bradford Dabbs <brad.dabbs@criticalstack.com>
2016-11-15 09:11:33 -0500

    Set script home and add comments

--- setup.sh
+++ setup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# Store where the script was called from so we can reference it later
+script_home="$(dirname "$0")"
+
+# Update bash-it if it's already installed or download it if it's not
+if [ -d $HOME/.bash_it ]; then
+  bash-it update
+else
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
+fi
+
+# Symlink all of our dotfiles to the home directory
 for i in .vimrc .vim .dircolors;
 do
-  ln --symbolic --interactive $PWD/$i $HOME/$i
+  ln --symbolic --interactive $script_home/$i $HOME/$i
 done
 
-if [ $OSTYPE == darwin* ]; then
-  ln --symbolic --interactive $PWD/.bashrc $HOME/.bash_profile
+# Adjust the .bashrc path if we're running on OSX
+if [[ $OSTYPE == darwin* ]]; then
+  ln --symbolic --interactive $script_home/.bashrc $HOME/.bash_profile
 else
-  ln --symbolic --interactive $PWD/.bashrc $HOME/.bashrc
+  ln --symbolic --interactive $script_home/.bashrc $HOME/.bashrc
 fi
