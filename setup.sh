ca5fe920020930b46b52025034b58d1126dd6fbf
Bradford Dabbs <brad.dabbs@criticalstack.com>
2016-11-16 13:02:32 -0500

    Add parameters and download vim files directly
    No point in symlinking a point-in-time snapshot of the vim files. Just
    download the most current version from the maintainers.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+params="-sf"
+
+while getopts "vib" args; do
+    case $args in
+        v)
+            params="$params -v"
+            ;;
+        i)
+            params="$params -i"
+            ;;
+        b)
+            params="$params -b"
+            ;;
+    esac
+done
+
 # Store where the script was called from so we can reference it later
 script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -11,15 +27,27 @@ else
   git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
 fi
 
+# Add vim-plug if it doesn't already exist
+if [ ! -f $HOME/.vim/autoload/plug.vim ]; then
+    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Add solarized colors for vim if not present
+if [ ! -f $HOME/.vim/colors/solarized.vim ]; then
+    curl -fLo $HOME/.vim/colors/solarized.vim --create-dirs \
+    https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+fi
+
 # Symlink all of our dotfiles to the home directory
-for i in .vimrc .vim .dircolors;
+for i in .vimrc .dircolors;
 do
-  ln --symbolic --interactive $script_home/$i $HOME/$i
+  ln $params $script_home/$i $HOME/$i
 done
 
 # Adjust the .bashrc path if we're running on OSX
 if [[ $OSTYPE == darwin* ]]; then
-  ln --symbolic --interactive $script_home/.bashrc $HOME/.bash_profile
+  ln $params $script_home/.bashrc $HOME/.bash_profile
 else
-  ln --symbolic --interactive $script_home/.bashrc $HOME/.bashrc
+  ln $params $script_home/.bashrc $HOME/.bashrc
 fi
