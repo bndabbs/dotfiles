46592e8cc8eff7602a499e733d08397991571588
Bradford Dabbs <brad@criticalstack.com>
2017-01-13 11:32:27 -0600

    Add gitdone function to make git life easier

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -5,6 +5,9 @@ alias egrep="egrep --color=always"
 alias gc="git commit -S -v"
 alias gcm="git commit -S -v -m"
 
+# Commit and push everything
+gitdone() { git add -A; git commit -m "$1"; git push; }
+
 alias bro-column="sed \"s/fields.//;s/types.//\" | column -s $'\t' -t"
 alias bro-awk='awk -F" "'
 bro-grep() { grep -E "(^#)|$1" $2; }
